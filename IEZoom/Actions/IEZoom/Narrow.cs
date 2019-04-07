9e5487317f9be8113d3c858ec8c766e81eaad311
basyura <basyura@gmail.com>
2019-04-07 15:11:06 +0900

    テキスト入力中に絞り込む

---
+++ IEZoom/Actions/IEZoom/Narrow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Eleve;
+
+namespace IEZoom.Actions.IEZoom
+{
+    public class Narrow : IEZoomActionBase
+    {
+        public override Task<ActionResult> Execute(object sender, EventArgs args, object obj)
+        {
+            KeyEventArgs evnt = args as KeyEventArgs;
+            if (evnt.Key == Key.Enter)
+            {
+                Keyboard.ClearFocus();
+            }
+
+            ExecuteCommand<Refresh>();
+
+            return SuccessTask;
+        }
+    }
+}
