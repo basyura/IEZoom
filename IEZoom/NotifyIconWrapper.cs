a7d0e08155ecc5251a8b2314cf9df27494b0394b
basyura <basyura@gmail.com>
2021-01-29 13:24:56 +0900

    activate window

--- IEZoom/NotifyIconWrapper.cs
+++ IEZoom/NotifyIconWrapper.cs
@@ -56,14 +56,8 @@ namespace IEZoom
             }
 
             View.Visibility = Visibility.Visible;
-            View.Topmost = true;
-            View.Height -= 1;
             View.UpdateLayout();
-            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
-            {
-                View.Topmost = false;
-                View.Height += 1;
-            }));
+            View.Activate();
         }
         /// <summary>
         /// 
